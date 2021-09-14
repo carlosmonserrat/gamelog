3443f3000287f6fa32dd4cab88d104169349b874
Carlos Rojas <soy_solamente@hotmail.com>
2021-09-15 01:50:07 +0300

    start script

--- Assets/Artwork/Player.cs
+++ Assets/Artwork/Player.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private BoxCollider2D boxCollider;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Start()
+   {
+       boxCollider = GetComponent<BoxCollider2D>();
+   }
+
+  
 }
